855bdba3c1dc976803c196e863fe470fe207822b
Xstyeven <m18500359468@163.com>
2017-04-10 11:10:01 +0800

    Main

--- app/src/main/java/com/steven/stevenzhibo/MainActivity.java
+++ app/src/main/java/com/steven/stevenzhibo/MainActivity.java
@@ -1,13 +1,17 @@
 package com.steven.stevenzhibo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(mContext,"ssssssssss",Toast.LENGTH_LONG).show();
     }
 }
